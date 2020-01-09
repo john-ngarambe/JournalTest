1351eb9a7562ed111076868be25d41b066926779
john-ngarambe <51552292+john-ngarambe@users.noreply.github.com>
2020-01-09 00:05:30 -0500

    constructor added

--- cording/src/gitthings/Personal.java
+++ cording/src/gitthings/Personal.java
@@ -1,4 +1,28 @@
 package gitthings;
 
 public class Personal {
+
+    private String name;
+    private int age;
+
+    public Personal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
